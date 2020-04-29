8b651ccca6db8b36bfb65bbeab96f6904b9fba23
Meshi Fried <meshifried@gmail.com>
2020-04-29 10:06:43 -0700

    Create DiceTest.java

---
+++ src/test/java/com/amdocs/monopoly/test/DiceTest.java
@@ -0,0 +1,25 @@
+package com.amdocs.monopoly.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amdocs.monopoly.production.Dice;
+
+public class DiceTest {
+
+	@Test
+	public void DiceExist() {
+		Dice dice = new Dice();
+		
+		Assert.assertNotNull(dice);
+	}
+	
+	@Test
+	public void DiceReturnsNumber() {
+		Dice dice = new Dice();
+		int num = dice.randomNumberGenerator();
+		System.out.println(num);
+		Assert.assertTrue((num <= 6 && num >=1));
+	}
+
+}
