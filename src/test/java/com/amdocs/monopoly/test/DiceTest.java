37c0f33584e59c8924058a257288a298141786c7
Meshi Fried <meshifried@gmail.com>
2020-04-29 13:47:02 -0700

    Update DiceTest.java

--- src/test/java/com/amdocs/monopoly/test/DiceTest.java
+++ src/test/java/com/amdocs/monopoly/test/DiceTest.java
@@ -17,9 +17,20 @@ public class DiceTest {
 	@Test
 	public void DiceReturnsNumber() {
 		Dice dice = new Dice();
+<<<<<<< Updated upstream
 		int num = dice.randomNumberGenerator();
 		System.out.println(num);
+=======
+		int num = dice.roll();
+>>>>>>> Stashed changes
 		Assert.assertTrue((num <= 6 && num >=1));
+	}
+	
+	@Test
+	public void DiceGetSteps() {
+		Dice dice = new Dice();
+		int num = dice.getSteps();
+		Assert.assertTrue((num <= 12 && num >=2));
 	}
 
 }
